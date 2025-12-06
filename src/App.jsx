868c0299993374f0aafe94bb46203cf9ef6bcb07
firas1996 <firas.hm.1996@gmail.com>
2025-12-06 10:48:10 +0100

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "./components/Login";
 
 const App = () => {
   const myName = "Firas";
+  const [x, setX] = useState("");
   const getData = (data) => {
-    console.log(data);
+    setX(data);
   };
   return (
     <>
-      <h1>Hello, ...... </h1>
+      <h1>Hello, {x} </h1>
       <Login myName={myName} getData={getData} />
     </>
   );
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,5 +2,14 @@ const Login = ({ myName, getData }) => {
   const abc = "World !";
   getData(abc);
   return <p>My name is {myName} </p>;
+  //   return (
+  //     <>
+  //       <label>Email</label>
+  //       <input type="email" />
+  //       <label>Password</label>
+  //       <input type="password" />
+  //       <button>Login</button>
+  //     </>
+  //   );
 };
 export default Login;
